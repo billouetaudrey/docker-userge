65fc7688520e52d753dcb8767d49a5d93eb4970a
Billou <32905881+billouetaudrey@users.noreply.github.com>
2022-03-02 10:48:04 +0100

    Create setup_docker.sh

---
+++ setup_docker.sh
@@ -0,0 +1,13 @@
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+#Optional if you don't want use sudo for launch docker, replace yourname by your home
+
+#sudo usermod -aG docker yourname
+
+#Optional if you need  to install pip
+
+#sudo apt-get install python3-distutils python3-dev libffi-dev libssl-dev
+#curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+#sudo python3 get-pip.py
+#sudo pip3 install docker-compose
